63c61b61111a44d6c5a1181ba066929a6a2effed
Sergei Antipov <mrantip90@gmail.com>
2023-10-29 15:36:51 +0300

    v3 Поменял палку (f"resourses/{file_name}"))

--- resource.py
+++ resource.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 
 def path(file_name):
-    return str(Path(__file__).parent.joinpath(f"resourses\{file_name}"))
+    return str(Path(__file__).parent.joinpath(f"resourses/{file_name}"))
